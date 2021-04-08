6f8947dea7448a8a4f491c45d602dd907bb08d8b
HeetShah <heetshah7642@gmail.com>
2021-04-07 18:30:40 -0600

    unit 4, lesson 4, codiing activity 1

---
+++ Unit 5/Lesson 4/U5_L4_Activity_One.java
@@ -0,0 +1,20 @@
+/* Lesson 4 Coding Activity Question 1 */
+
+import java.util.Scanner;
+import edhesive.shapes.*;
+
+public class U5_L4_Activity_One {
+
+    /* Add the method areaSum here */
+    public static double areaSum(Circle x, Circle y) {
+        return x.getArea() + y.getArea();
+    }
+    // You can uncomment and add to the main method to test your code
+    // You will need to remove/comment out this method before checking your code for
+    // a score
+    /*
+     * public static void main(String[] args){
+     * 
+     * }
+     */
+}
