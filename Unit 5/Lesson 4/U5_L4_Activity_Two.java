4d0871be696fdf17786803bd99740187b87686ac
HeetShah <heetshah7642@gmail.com>
2021-04-07 19:28:20 -0600

    finished unit 5 lesson 4

---
+++ Unit 5/Lesson 4/U5_L4_Activity_Two.java
@@ -0,0 +1,21 @@
+/* Lesson 4 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+public class U5_L4_Activity_Two {
+
+    /* Add the method average here */
+    public static double average(int a, int b, int c, int d, int e) {
+        return ((double) (a + b + c + d + e)) / 5;
+
+    }
+
+    // You can uncomment and add to the main method to test your code
+    // You will need to remove/comment out this method before checking your code for
+    // a score
+
+    // public static void main(String[] args){
+    // System.out.println(average(1, 5, 7, 4, 10));
+    // }
+
+}
